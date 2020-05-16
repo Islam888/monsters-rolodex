a87dfb8bda066b29231f4c5f560159a777b7bdb2
Islam Sayed <islam.sayed8@gmail.com>
2020-05-16 17:31:09 +0200

    Updates

---
+++ precache-manifest.f265eeedcaaa4e039cb1e43f2072db42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806f6df1acc7d33351c6f18ab6886d49",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1f668461bd4257ee5cff",
+    "url": "/monsters-rolodex/static/css/main.46d5bff9.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f668461bd4257ee5cff",
+    "url": "/monsters-rolodex/static/js/main.f53bd2c4.chunk.js"
+  },
+  {
+    "revision": "46fcc5578023f1586c35",
+    "url": "/monsters-rolodex/static/js/runtime-main.7affd505.js"
+  }
+]);
